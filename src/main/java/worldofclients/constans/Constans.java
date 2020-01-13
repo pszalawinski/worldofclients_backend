1e0b4b8fdb42e56c5fe509c3e244c563734b6174
Pawel Szalawinski <pawelszalawinski@gmail.com>
2020-01-13 09:18:47 +0100

    for github publication

--- src/main/java/worldofclients/constans/Constans.java
+++ src/main/java/worldofclients/constans/Constans.java
@@ -2,7 +2,7 @@ package worldofclients.constans;
 
 public class Constans {
 
-    public static final String API_KEY = "&key=AIzaSyC01YfHyvWtoGSemJFaTeEdXgQAPBsU3vY";
+    public static final String API_KEY = ""; // TODO: 13/01/2020 Here your google api key 
     public static final String GEO_SERVICE = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 
 }
